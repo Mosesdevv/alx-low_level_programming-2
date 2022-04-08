fa040fe2d6ef96ad9e875c1939fc7cd15091f70a
Your Name <you@example.com>
2022-04-08 11:08:03 -0700

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+i#include <stdlib.h>
+#include <time.h> 
+#include <stdio.h>   
+/**
+ * main - Prints a random number and states whether  
+ * it is positive , negative or zero. 
+ *
+ * Return: Always 0. 
+ */
+int main(void)                                                                                                                          
+
+	                                                                                                                                        
+
+{                                                                                                                                       
+
+	int n;  
+
+	srand(time(0));                                                                                                                         
+
+	n = rand() - RAND_MAX / 2;                                                                                                              
+
+	                                                                                                                                        
+
+	printf("Last digit of %d is %d %s\n", n, n % 10,
+
+			((n % 10) == 0) ? "and is 0"
+
+			: (((n % 10) > 5) ? "and is greater than 5"
+
+				:a                                                                                                             
+
+				return(0);                                                                                                                              
+
+				}         
