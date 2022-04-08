2859759780f9472b723c4dab3497915035b0f396
Your Name <you@example.com>
2022-04-08 10:02:36 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,14 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include <time.h>
 
 /* more headers goes there */
-
-
 
 /* betty style doc for function main goes there */
 
 int main(void)
 
 {
-
 		int n;
-
-
 
 			srand(time(0));
 
