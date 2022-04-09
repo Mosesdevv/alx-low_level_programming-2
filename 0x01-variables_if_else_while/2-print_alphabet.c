40d0fd2782647ac6659392397033f20515f08300
Your Name <you@example.com>
2022-04-09 07:21:36 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>                                                                                                                     
+#include <time.h>
+/**
+ *
+ * main - Prints the lower alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+putchar("a" + c);
+
+putchar("\n");
+
+return (0);
+}
