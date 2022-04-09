c4842c63b9b46567227b38cca11f5baf24a45c49
Your Name <you@example.com>
2022-04-09 06:38:18 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,28 +7,16 @@ i#include <stdlib.h>
  *
  * Return: Always 0. 
  */
-int main(void)                                                                                                                          
+int main(void)
+{
+int n;
 
-	                                                                                                                                        
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-{                                                                                                                                       
-
-	int n;  
-
-	srand(time(0));                                                                                                                         
-
-	n = rand() - RAND_MAX / 2;                                                                                                              
-
-	                                                                                                                                        
-
-	printf("Last digit of %d is %d %s\n", n, n % 10,
-
-			((n % 10) == 0) ? "and is 0"
-
-			: (((n % 10) > 5) ? "and is greater than 5"
-
-				:a                                                                                                             
-
-				return(0);                                                                                                                              
-
-				}         
+printf("Last digit of %d is %d %s\n", n, n % 10,
+((n % 10) == 0) ? "and is 0"
+: (((n % 10) > 5) ? "and is greater than 5"
+:a
+return(0);
+}
