e322a38c1bb1f13c57c2b865dc661aa288616da1
Your Name <you@example.com>
2022-04-09 08:05:17 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Description - Prints lower alphabet
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 0; c < 26; ++c)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
