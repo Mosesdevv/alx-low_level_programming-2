e19d8966ec6afc75342cf63f671493c0b810f89d
Your Name <you@example.com>
2022-04-12 15:29:12 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**                                                                                                                                     
  * main - Prints a program to return 0
  * Return:This program returns 0
